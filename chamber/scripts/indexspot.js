6c786f93e02f5e53d96d7764179762fe0c617ab8
jasjade <dod16008@byui.edu>
2022-11-24 12:47:59 -0500

    change json

--- chamber/scripts/indexspot.js
+++ chamber/scripts/indexspot.js
@@ -3,56 +3,59 @@
 const requestURL = 'https://jasjade.github.io/newwdd230/chamber/data.json';
 const cards = document.querySelector('#cards');
 
-//the fetch() method requires at least one argument, the path to the resource (the URL of the JSON file)
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    //store the results of the converted response into an array 
-    const companies = jsonObject['companies'];
-    //const membership = jsonObject['membershipLevel'];
-    //var result = jsonObject.filter((x)=>x.membershipLevel === "gold");
-    //console.log(result);
-  });
+async function spotLights() {
+  try {
+    let jsonBusinessFile = 'https://raw.githubusercontent.com/samgo23/wdd230/main/chamber/data.json';
+
+    const response = await fetch(jsonBusinessFile);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call 
+
+      const businesses = data['businesses'];
+      businesses.forEach(spotLight);
 
 
+    } else {
+      throw Error(await response.text());
+    };
 
-function displayCompanies(companies) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-    let p = document.createElement('p');
-    let p2 = document.createElement('p');
-    let link = document.createElement('a')
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${companies.name}`;
-    p.textContent = ` ${companies.addresses}`;
-    p2.textContent = `${companies.phoneNumbers}`;
-    link.textContent = `${companies.websiteURLs}`;
-    link.setAttribute('href', companies.websiteURLs);
+    function spotLight(business) {
+      if (business.memership_level >= 2) {
+        console.log('works')
+        let div = document.createElement('div');
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let logo = document.createElement('img');
+        let contact = document.createElement('div');
+        let phoneNumber = document.createElement('p');
+        let address = document.createElement('p');
 
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', companies.logo);
-    portrait.setAttribute('alt', `${companies.name} logo`);
-    portrait.setAttribute('loading', 'lazy');
-  
-    // Add/append the section(card) with the h2 element
-    card.appendChild(portrait);
-    card.appendChild(h2);
-    card.appendChild(p);
-    card.appendChild(p2);
-    card.appendChild(link);
-    
-    
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div#cards').appendChild(card);
+
+        // Business info
+        h2.textContent = (`${business.name}`);
+        phoneNumber.textContent = (`${business.phone_number}`)
+        address.textContent = (`${business.address}`)
+
+        //Buisness logo
+        logo.setAttribute('src', business.imageurl);
+        logo.setAttribute('alt', (`${business.name} logo`))
+        logo.setAttribute('loading', 'lazy')
+
+        // append to card
+        card.appendChild(h2);
+        card.appendChild(logo);
+        card.appendChild(contact);
+        contact.appendChild(phoneNumber);
+        contact.appendChild(address);
+
+
+        // append to doc
+        document.querySelector('.spotlight').appendChild(card);
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-
-
+};
+spotLights();
