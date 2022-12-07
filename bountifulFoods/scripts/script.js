f6e32e6d8715f8ab1a1ea5fc2c7fe322c60ef455
jasjade <dod16008@byui.edu>
2022-12-07 11:02:47 -0500

    changed name

---
+++ bountifulFoods/scripts/script.js
@@ -0,0 +1,9 @@
+//nav menu
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open"); //adds class of "open" to nal ul when clicked works like a light switch adding when clicked then unadding when its clicked again
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("fullNav").classList.toggle("open");
+    document.querySelector("header").classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
